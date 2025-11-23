87031db4e54972d9a13a9d54a97d34c8d94d126e
Joyce Yang <yangjoyce02@gmail.com>
2025-11-22 23:35:51 -0800

    implemented clear button

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,12 @@
 import {useState, useEffect} from 'react'
 import './SearchBar.css'
 import searchIcon from '../assets/search.svg'
+import clearButton from '../assets/clear-button.svg'
 
 function SearchBar({onSearch, onSearchBarClick}) {
     const [searchQuery, setSearchQuery] = useState("");
     const [searchInput, setSearchInput] = useState("");
+    const [clearBtnDisplay, setClearBtnDisplay] = useState({display: "none;"})
 
     useEffect(() => {
         onSearch(searchQuery);
@@ -13,28 +15,37 @@ function SearchBar({onSearch, onSearchBarClick}) {
     function handleInput(e) {
         setSearchQuery(e.target.value);
         setSearchInput(e.target.value);
+        setClearBtnDisplay({display: "block"});
     }
 
-    function clearInput(e) {
+    function clearInput() {
         setSearchQuery("");
         setSearchInput("");
+        document.getElementById("search-input-area").focus();
+        setClearBtnDisplay({display: "none"});
+    }
+
+    function handleEnter(e) {
+        if (e.key === "Enter") {
+            document.getElementById("search-input-area").blur();
+        }
     }
 
     return (
         <>
             <div className="search-bar">
-                <img className="search-icon" alt="Search Bar" src={searchIcon} />
+                <img className="search-icon" alt="Search Icon" src={searchIcon} />
                 <input 
                     id="search-input-area"
                     placeholder="Search"
                     value={searchInput}
+                    onKeyDown={(e) => handleEnter(e)}
                     onInput={(e) => handleInput(e)}
                     onClick={onSearchBarClick}
                     inputMode="none">
                 </input>
-                <button className="clear-search-btn"
-                    onClick={(e) => clearInput(e)}>
-                        X
+                <button className="clear-search-btn" onClick={clearInput} style={clearBtnDisplay}>
+                    <img alt="Clear Input" src={clearButton}/>
                 </button>
             </div>
         </>
